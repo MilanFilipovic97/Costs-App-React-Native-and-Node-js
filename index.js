4449408bb306c22c5cddfdae3a9ccb50e0439125
slavni <slavnipokusaj@gmail.com>
2020-06-18 14:08:43 +0200

    to je to

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const cors = require('cors');
+//const cors = require('cors');
 const express = require('express');
 const mysql = require('mysql');
-require('dotenv').config();
+//require('dotenv').config();
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
